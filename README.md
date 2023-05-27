1cd5a8d1ba124628ef535397307253ecba4c20ad
JerryI <krikus.ms@gmail.com>
2023-05-27 23:36:36 +0200

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# WLJS JS support package
+**An extension for [WLJS-Editor](https://github.com/JerryI/wljs-editor) to add JS type of cells...**
+
+## License
+Project is released under the GNU General Public License (GPL).
---
+++ dist/kernel.js
@@ -0,0 +1,76 @@
+function __isElement(element) {
+    return element instanceof Element || element instanceof HTMLDocument;  
+  }
+  
+class ScopedEval {
+  ondestroy = function() {}
+  error  = false
+    
+  constructor(scope, script) {
+    this.script = '(() => {'+ script + '})()';
+  }
+    
+  eval() {
+    try {
+      return eval(this.script);
+    } catch(err) {
+      this.error = err;
+    }
+  }
+} 
+
+class JSCell {
+    scope = {}
+    createScopedEval = (scope, script) => {return({
+      ondestroy: function() {},
+      result: Function(`${script}`)
+    })}  
+    
+    dispose() {
+      this.scope.ondestroy();
+    }
+    
+    constructor(parent, data) {
+      this.origin = parent;
+      this.scope = new ScopedEval({}, data);
+      
+      const result = this.scope.eval();
+
+      if (this.scope.error) {
+        const errorDiv = document.createElement('div');
+        errorDiv.innerText = this.scope.error;
+        errorDiv.classList.add('err-js');
+        this.origin.element.appendChild(errorDiv);
+        return this;
+      }
+
+      if (__isElement(result)) {
+        this.origin.element.appendChild(result);
+        return this;
+      }
+      
+      new window.EditorView({
+        doc: String(result),
+        extensions: [
+          window.highlightSpecialChars(),
+          window.EditorState.readOnly.of(true),
+          window.javascript(),
+          window.syntaxHighlighting(window.defaultHighlightStyle, { fallback: true }),
+          window.editorCustomTheme
+        ],
+        parent: this.origin.element
+      });    
+      
+      return this;
+    }
+  }
+  
+  window.SupportedLanguages.push({
+    check: (r) => {return(r[0] === '.js')},
+    plugins: [window.javascript()],
+    name: window.javascriptLanguage.name
+  });
+
+  window.SupportedCells['js'] = {
+    view: JSCell
+  };
